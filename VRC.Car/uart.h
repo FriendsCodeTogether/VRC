d9137a0d2e5682681cb905cb18b80d1c21e1c4c4
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-11-12 16:57:55 +0100

    finished code cleanup

---
+++ VRC.Car/uart.h
@@ -0,0 +1,20 @@
+/*
+ * uart.h
+ *
+ * Created: 2/11/2020 14:02:08
+ *  Author: VRCar-Team
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+
+void uartSetup(void);
+
+void uartSendByte(char value);
+
+void uartSendString(char *txt);
+
+int stringCompare(char * pnt1,char * pnt2);
+
+#endif /* UART_H_ */
