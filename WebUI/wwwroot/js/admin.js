68000b2c0047b8dfd52b3d0a3d4759547a977236
Tim Blokken <tim_blokken@hotmail.com>
2020-12-03 16:14:52 +0100

    added admin race buttons

---
+++ WebUI/wwwroot/js/admin.js
@@ -0,0 +1,29 @@
+var userId = getCookieValue('AnonymousUserId');
+var prepareRaceButton = document.getElementById("prepare-race-btn");
+var startRaceButton = document.getElementById("start-race-btn");
+var stopRaceButton = document.getElementById("stop-race-btn");
+const connection = new signalR.HubConnectionBuilder().withUrl('/racinghub').configureLogging(signalR.LogLevel.Information).build();
+
+
+
+prepareRaceButton.addEventListener("click", () => prepareRaceButtonClicked());
+startRaceButton.addEventListener("click", () => startRaceButtonClicked());
+stopRaceButton.addEventListener("click", () => stopRaceButtonClicked());
+
+function prepareRaceButtonClicked() {
+  prepareRaceButton.setAttribute("disabled", true);
+  startRaceButton.removeAttribute("disabled");
+
+}
+
+function startRaceButtonClicked() {
+  startRaceButton.setAttribute("disabled", true);
+  stopRaceButton.removeAttribute("disabled");
+
+}
+
+function stopRaceButtonClicked() {
+  stopRaceButton.setAttribute("disabled", true);
+  prepareRaceButton.removeAttribute("disabled");
+
+}
