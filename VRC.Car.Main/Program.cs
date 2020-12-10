f220aa4b0791f9a684a570e4def347297ab164f0
Joren Thijs <joren.thijs@hotmail.com>
2020-12-11 00:27:13 +0100

    added i2C to raspberrypi

--- VRC.Car.Main/Program.cs
+++ VRC.Car.Main/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Threading.Tasks;
 using VRC.Car.Main.Messaging;
 using VRC.Shared.Messaging;
+using System.Device.I2c;
 
 namespace VRC.Car.Main
 {
@@ -11,15 +12,22 @@ namespace VRC.Car.Main
     {
         static async Task Main(string[] args)
         {
-            var messagingHandler = new MessagingHandler();
-            await messagingHandler.ConnectAsync();
+            // var messagingHandler = new MessagingHandler();
+            // await messagingHandler.ConnectAsync();
 
-            var command = new CarCommand
-            {
-                CarNumber = messagingHandler.CarNumber,
-                Throttle = 1,
-                Direction = -1
-            };
+            // var command = new CarCommand
+            // {
+            //     CarNumber = messagingHandler.CarNumber,
+            //     Throttle = 1,
+            //     Direction = -1
+            // };
+
+            Console.WriteLine("Hello I2C!");
+            I2cDevice i2c = I2cDevice.Create(new I2cConnectionSettings(1, 0x20));
+            i2c.WriteByte(0x60);
+            //var read = i2c.ReadByte();
+            // Console.WriteLine(read);
+
             while (true)
             {
                 await Task.Delay(2000);
