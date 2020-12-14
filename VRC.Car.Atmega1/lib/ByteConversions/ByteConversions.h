ad9044b7a681d849dce304535821f3a6f6774e18
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-12-15 00:42:25 +0100

    fixed formatting

--- VRC.Car.Atmega1/lib/ByteConversions/ByteConversions.h
+++ VRC.Car.Atmega1/lib/ByteConversions/ByteConversions.h
@@ -1,7 +1,7 @@
 /*
  * ByteConversions.h
  *
- */ 
+ */
 
 
 #ifndef BYTE_CONVERSIONS_H_
@@ -10,9 +10,9 @@
 #include <avr/io.h>     /* Include AVR std. library file */
 #include <string.h>     /* Include string header file */
 
-void intToByteArray(int val,uint8_t* bytes_array);          /* Copy int value into byte[] */
-void longToByteArray(long val,uint8_t* bytes_array);		/* Copy long value into byte[] */
+void intToByteArray(int val, uint8_t* bytes_array);         /* Copy int value into byte[] */
+void longToByteArray(long val, uint8_t* bytes_array);		    /* Copy long value into byte[] */
 void floatToByteArray(float val, uint8_t* bytes_array);	    /* Copy double value into byte[] */
-void doubleToByteArray(double val, uint8_t* bytes_array);	/* Copy double value into byte[] */			
+void doubleToByteArray(double val, uint8_t* bytes_array);	  /* Copy double value into byte[] */
 
 #endif /* BYTE_CONVERSIONS_H_ */
