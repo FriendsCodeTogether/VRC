4fb5babff6f72bc93d9c636693e0e1895961969a
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-12-15 00:46:36 +0100

    fixed formatting

--- VRC.Car.Atmega1/lib/I2cSlave/I2cSlave.h
+++ VRC.Car.Atmega1/lib/I2cSlave/I2cSlave.h
@@ -1,7 +1,7 @@
 /*
  * I2cSlave.h
  *
- */ 
+ */
 
 
 #ifndef I2C_SLAVE_H_
@@ -13,8 +13,8 @@
 #define SDA 1
 
 void I2cSlaveInit(uint8_t slave_address);	/* I2C slave initialize function with Slave address */
-int8_t I2cSlaveListen();					/* I2C slave listen function */
-int8_t I2cSlaveTransmit(char data);			/* I2C slave transmit function */
-char I2cSlaveReceive();						/* I2C slave receive function */
+int8_t I2cSlaveListen();					        /* I2C slave listen function */
+int8_t I2cSlaveTransmit(char data);			  /* I2C slave transmit function */
+char I2cSlaveReceive();						        /* I2C slave receive function */
 
 #endif /* I2C_SLAVE_H_ */
