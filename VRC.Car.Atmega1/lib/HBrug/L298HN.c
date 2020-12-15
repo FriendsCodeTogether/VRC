66cc3d062913755afee165ee1562744cb2fb9794
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-12-15 20:01:00 +0100

    formatting error

--- VRC.Car.Atmega1/lib/HBrug/L298HN.c
+++ VRC.Car.Atmega1/lib/HBrug/L298HN.c
@@ -10,119 +10,125 @@
 
 void HBridgeSetup(void)
 {
-    DDRB = 0xff;
-    DDRD = DDRD | (1 << rightWheelsEnable);
-    pwmSetup();
+  DDRB = 0xff;
+  DDRD = DDRD | (1 << rightWheelsEnable);
+  pwmSetup();
 }
 
 void driveCar(char direction, char throttle, int speed)
 {
-    pwmSpeed(speed);
+  pwmSpeed(speed);
 
-    if (throttle == 'N')
-    {
-        stopCar();
-    }
-    else if (direction == 'N' && throttle == 'F')
-    {
-        driveStraightForwards();
-    }
-    else if (direction == 'N' && throttle == 'B')
-    {
-        driveStraightBackwards();
-    }
-    else if (direction == 'L' && throttle == 'F')
-    {
-        driveLeftForwards();
-    }
-    else if (direction == 'L' && throttle == 'B')
-    {
-        driveLeftBackwards();
-    }
-    else if (direction == 'R' && throttle == 'F')
-    {
-        driveRightForwards();
-    }
-    else if (direction == 'R' && throttle == 'B')
-    {
-        driveRightBackwards();
-    }
+  if (throttle == 'N')
+  {
+    stopCar();
+  }
+  else if (direction == 'N' && throttle == 'F')
+  {
+    driveStraightForwards();
+  }
+  else if (direction == 'N' && throttle == 'B')
+  {
+    driveStraightBackwards();
+  }
+  else if (direction == 'L' && throttle == 'F')
+  {
+    driveLeftForwards();
+  }
+  else if (direction == 'L' && throttle == 'B')
+  {
+    driveLeftBackwards();
+  }
+  else if (direction == 'R' && throttle == 'F')
+  {
+    driveRightForwards();
+  }
+  else if (direction == 'R' && throttle == 'B')
+  {
+    driveRightBackwards();
+  }
 }
 
 void stopCar(void)
 {
-    PORTB = PORTB & ~(1 << leftWheelsForwards);
-    PORTB = PORTB & ~(1 << rightWheelsForwards);
+  PORTB = PORTB & ~(1 << leftWheelsForwards);
+  PORTB = PORTB & ~(1 << rightWheelsForwards);
 
-    PORTB = PORTB & ~(1 << leftWheelsBackwards);
-    PORTB = PORTB & ~(1 << rightWheelsBackwards);
+  PORTB = PORTB & ~(1 << leftWheelsBackwards);
+  PORTB = PORTB & ~(1 << rightWheelsBackwards);
 
-    PINB = PINB & ~(1 << leftWheelsEnable);
-    PIND = PIND & ~(1 << rightWheelsEnable);
+  PINB = PINB & ~(1 << leftWheelsEnable);
+  PIND = PIND & ~(1 << rightWheelsEnable);
 }
 
 void driveStraightForwards(void)
 {
-    PORTB = PORTB | (1 << leftWheelsForwards);
-    PORTB = PORTB | (1 << rightWheelsForwards);
+  PORTB = PORTB | (1 << leftWheelsForwards);
+  PORTB = PORTB | (1 << rightWheelsForwards);
 
-    PORTB = PORTB & ~(1 << leftWheelsBackwards);
-    PORTB = PORTB & ~(1 << rightWheelsBackwards);
+  PORTB = PORTB & ~(1 << leftWheelsBackwards);
+  PORTB = PORTB & ~(1 << rightWheelsBackwards);
 
-    PINB = PINB | (1 << leftWheelsEnable);
-    PIND = PIND | (1 << rightWheelsEnable);
+  PINB = PINB | (1 << leftWheelsEnable);
+  PIND = PIND | (1 << rightWheelsEnable);
 }
 
 void driveStraightBackwards(void)
 {
-    PORTB = PORTB & ~(1 << leftWheelsForwards);
-    PORTB = PORTB & ~(1 << rightWheelsForwards);
+  PORTB = PORTB & ~(1 << leftWheelsForwards);
+  PORTB = PORTB & ~(1 << rightWheelsForwards);
 
-    PORTB = PORTB | (1 << leftWheelsBackwards);
-    PORTB = PORTB | (1 << rightWheelsBackwards);
+  PORTB = PORTB | (1 << leftWheelsBackwards);
+  PORTB = PORTB | (1 << rightWheelsBackwards);
 
-    PINB = PINB | (1 << leftWheelsEnable);
-    PIND = PIND | (1 << rightWheelsEnable);
+  PINB = PINB | (1 << leftWheelsEnable);
+  PIND = PIND | (1 << rightWheelsEnable);
 }
 
 driveLeftForwards()
 {
-    PORTB = PORTB & ~(1 << leftWheelsForwards);
-    PORTB = PORTB | (1 << rightWheelsForwards);
+  PORTB = PORTB & ~(1 << leftWheelsForwards);
+  PORTB = PORTB | (1 << rightWheelsForwards);
 
-    PORTB = PORTB & ~(1 << leftWheelsBackwards);
-    PORTB = PORTB & ~(1 << rightWheelsBackwards);
+  PORTB = PORTB & ~(1 << leftWheelsBackwards);
+  PORTB = PORTB & ~(1 << rightWheelsBackwards);
 
-    PINB = PINB & ~(1 << leftWheelsEnable);
-    PIND = PIND | (1 << rightWheelsEnable);
+  PINB = PINB & ~(1 << leftWheelsEnable);
+  PIND = PIND | (1 << rightWheelsEnable);
 }
 
 driveLeftBackwards()
 {
-    PORTB = PORTB & ~(1 << leftWheelsForwards);
-    PORTB = PORTB & ~(1 << rightWheelsForwards);
-    PORTB = PORTB & ~(1 << leftWheelsBackwards);
-    PORTB = PORTB | (1 << rightWheelsBackwards);
-    PINB = PINB & ~(1 << leftWheelsEnable);
-    PIND = PIND | (1 << rightWheelsEnable);
+  PORTB = PORTB & ~(1 << leftWheelsForwards);
+  PORTB = PORTB & ~(1 << rightWheelsForwards);
+
+  PORTB = PORTB & ~(1 << leftWheelsBackwards);
+  PORTB = PORTB | (1 << rightWheelsBackwards);
+
+  PINB = PINB & ~(1 << leftWheelsEnable);
+  PIND = PIND | (1 << rightWheelsEnable);
 }
 
 driveRightForwards()
 {
-    PORTB = PORTB | (1 << leftWheelsForwards);
-    PORTB = PORTB & ~(1 << rightWheelsForwards);
-    PORTB = PORTB & ~(1 << leftWheelsBackwards);
-    PORTB = PORTB & ~(1 << rightWheelsBackwards);
-    PINB = PINB | (1 << leftWheelsEnable);
-    PIND = PIND & ~(1 << rightWheelsEnable);
+  PORTB = PORTB | (1 << leftWheelsForwards);
+  PORTB = PORTB & ~(1 << rightWheelsForwards);
+
+  PORTB = PORTB & ~(1 << leftWheelsBackwards);
+  PORTB = PORTB & ~(1 << rightWheelsBackwards);
+
+  PINB = PINB | (1 << leftWheelsEnable);
+  PIND = PIND & ~(1 << rightWheelsEnable);
 }
 
 driveRightBackwards()
 {
-    PORTB = PORTB & ~(1 << leftWheelsForwards);
-    PORTB = PORTB & ~(1 << rightWheelsForwards);
-    PORTB = PORTB | (1 << leftWheelsBackwards);
-    PORTB = PORTB & ~(1 << rightWheelsBackwards);
-    PINB = PINB | (1 << leftWheelsEnable);
-    PIND = PIND & ~(1 << rightWheelsEnable);
+  PORTB = PORTB & ~(1 << leftWheelsForwards);
+  PORTB = PORTB & ~(1 << rightWheelsForwards);
+
+  PORTB = PORTB | (1 << leftWheelsBackwards);
+  PORTB = PORTB & ~(1 << rightWheelsBackwards);
+
+  PINB = PINB | (1 << leftWheelsEnable);
+  PIND = PIND & ~(1 << rightWheelsEnable);
 }
