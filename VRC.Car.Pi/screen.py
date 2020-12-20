a8970a06378648196d2203f68ef7434cedf1552f
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-12-20 17:23:41 +0100

    added color set

--- VRC.Car.Pi/screen.py
+++ VRC.Car.Pi/screen.py
@@ -7,8 +7,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
 
 display.fill(0)
-display.text('Hello', 0, 0)
-display.text('World', 0, 10)
+display.text('Hello', 0, 0, 1)
+display.text('World', 0, 10, 1)
 display.show()
 
 while True:
