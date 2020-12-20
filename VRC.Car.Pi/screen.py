64aed78513aba2d05cfe1a7ac1e8c9419740e192
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-12-20 17:00:51 +0100

    fixed screen size

--- VRC.Car.Pi/screen.py
+++ VRC.Car.Pi/screen.py
@@ -3,7 +3,7 @@ import busio
 import adafruit_ssd1306
 
 i2c = busio.I2C(board.SCL, board.SDA)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c)
+display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
 
 display.fill(0)
 
